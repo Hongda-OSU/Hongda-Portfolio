9fed109513e167280b650a0de1bc61d7295c0ddd
Hongda-OSU <linhongda77@gmail.com>
2023-06-26 01:48:29 -0400

    update

--- src/data/resume/positions.js
+++ src/data/resume/positions.js
@@ -5,8 +5,9 @@ const positions = [
     link: '',
     daterange: '五月 2023 - 现在',
     points: [
-      '参与了一个名为 Pawstopia 的微信小程序的开发，该程序采用了 React，Taro，Redux，ASP.NET 等技术进行开发。',
-      '使用 UmiJS，DvaJS，和 Ant Design 等技术开发后台管理系统。',
+      '参与了 Pawstopia 宠物社交应用的开发。',
+      '为 Pawstopia 的全栈开发做出贡献，使用了如 ReactJS，Redux，Weixin Devtools，Taro，UmiJS，ASP.NET 等技术。',
+      '实现了各种功能，包括宠物图像上传，宠物声音录制和爪印扫描。',
     ],
   },
   {
@@ -15,10 +16,10 @@ const positions = [
     link: 'https://www.youtube.com/watch?v=l78ShugcO8w&t=52s',
     daterange: '一月 2023 - 四月 2023',
     points: [
-      '在软件开发毕业设计项目中参与建设一个名为 Affordable 的非营利医疗基金项目。',
-      '通过设计用户输入验证和动态密码创建反馈来改进登录和注册页面。',
-      '解决前后端在 AWS Cognito 和本地 MySQL 数据库上正确存储与更新电子邮件校验的问题。',
-      '设计 UI 测试脚本来模拟用户申请健康补助金，同时监测 Stripe 端能够进行正确汇款。',
+      '参与了非营利性医疗基金项目 Affordable 的开发。',
+      '通过输入检查和动态密码创建反馈改进了登录和注册页面。',
+      '解决了电子邮件验证问题，确保在 AWS Cognito 和本地 MySQL 数据库上都正确存储和更新了已验证的电子邮件。',
+      '设计了 UI 测试脚本以模拟用户申请健康福利，同时监视 Stripe 方面的正确转账。',
     ],
   },
   {
@@ -27,11 +28,11 @@ const positions = [
     link: 'https://github.com/Hongda-OSU/CSE5236-Buckeye-Schedule',
     daterange: '九月 2022 - 十二月 2022',
     points: [
-      '开发了一款能够帮助大学生安排日程的 Android 应用程序。',
-      '使用 MVVM 设计模式，Room 数据库，以及 Adapter 模式。',
-      '部署 Firestore 数据库服务并将用户数据保存在云端。',
-      '运用了许多手机内部功能，例如拍照功能和消息推送功能。',
-      '完成了不同的非功能性需求，包括手机权限检查和多语言切换和 UI 测试。',
+      '开发了一款 Android 应用程序，Buckeye Schedule，以帮助大学生维护他们的日程。',
+      '利用 MVVM 设计模式、Room 数据库和适配器模式进行高效的本地数据持久化和 UI 控制。',
+      '利用如 Firestore 的 NoSQL 数据库在云端保存用户数据。',
+      '启用手机内部服务，包括相机功能和通知服务。',
+      '结合非功能性需求，如用户权限检查、多语言支持和UI测试，以提升用户体验和应用性能。',
     ],
   },
   {
@@ -40,10 +41,10 @@ const positions = [
     link: 'https://hireez.com/',
     daterange: '五月 2022 - 八月 2022',
     points: [
-      '协助开发 Event Tracking System，用来帮助 BI 团队分析网站内的用户偏好和活动。',
-      '使用 Playwright 设计了一个 UI 自动化测试框架，用于对新的 Event Tracking System 进行测试。',
+      '参与开发事件跟踪系统，用来帮助 BI 团队分析网站内的用户偏好和活动。',
+      '使用 Playwright 为事件跟踪系统设计了一个自动化的 UI 测试框架。',
       '为 BI 团队开发了一个 Jira 工单自动化的 Web 应用，能够一次性完成工单的创建和推送。',
-      '负责 ELT 数据收集和日常网站维护。',
+      '通过从 Excel 转向 Jira 进行报告工具的转变，将 BI 团队的票据报告时间减少了40%，从而提高了团队的效率。',
     ],
   },
   {
@@ -53,9 +54,9 @@ const positions = [
     daterange: '一月 2022 - 五月 2022',
     points: [
       '在游戏开发毕业设计项目中制作一个名为 Karma 的 FPS & ARPG 游戏。',
-      '作为项目组长，每两周一次通过 Timebox 演示交流开发进度。',
-      '在 Unity 中开发了 FPS 系统、武器库系统、玩家控制脚本、敌人 AI 设计和动画设计。',
-      '在学期的毕设项目展示中公投被评为“最佳游戏”和“最精美的游戏”。',
+      '担任敏捷开发的 Scrum Master，确保通过时间盒展示进行双周进度沟通。',
+      '开发了核心游戏功能，包括 FPS 系统、物品栏系统、玩家控制、敌人 AI 以及动画系统。',
+      '在学期顶尖展示投票中，从七个班级团队中荣获 "The Best Game" 和 "The Most Polished Game" 奖项。',
     ],
   },
   {
@@ -64,9 +65,9 @@ const positions = [
     link: 'https://github.com/Hongda-OSU/CraftMine',
     daterange: '十一月 2021 - 十二月 2021',
     points: [
-      '学习在 Unity 中构建类似 Minecraft 的体素的世界。',
-      '运用 3D 属性、纹理映射、Perlin 噪声和数据序列化来生成世界。',
-      '实现了基本的用户交互，包括玩家控制和建筑系统。',
+      '作为一个自我驱动的学习项目，开发了一个类似于 Minecraft 的基于体素的世界。',
+      '利用了 3D 属性、纹理映射、Perlin 噪声和数据序列化技术，为复杂的世界生成提供支持。',
+      '制作了基本的玩家交互，包括导航系统和建筑系统，丰富了用户体验。',
     ],
   },
   {
@@ -75,9 +76,9 @@ const positions = [
     link: 'https://github.com/Hongda-OSU/CSE3541-Endless-Runner',
     daterange: '十一月 2021 - 十二月 2021',
     points: [
-      '在 Unity 中设计了一款跑酷游戏。',
-      '使用 Unity 新输入系统、Animator 和 Cinemachine 来获得更好的游戏体验。',
-      '设计关卡生成，包括程序化环境生成，障碍物放置，以及天空盒。',
+      '利用 Unity 设计和开发了一款吸引人的无限跑酷游戏。',
+      '通过应用 Unity 的新输入系统、动画系统和 Cinemachine 来增强游戏体验。',
+      '编程实现了动态关卡生成功能，包括环境变化和障碍物的放置。',
     ],
   },
   {
@@ -86,20 +87,9 @@ const positions = [
     link: 'https://github.com/Hongda-OSU/CSE-3902-Team-JellyLake-Zelda-',
     daterange: '八月 2021 - 十二月 2021',
     points: [
-      '还原了经典游戏塞尔达传说 1986 中的 level 1 地牢。',
-      '利用状态模式、装饰者模式和单例模式来设计不同的敌人和玩家能力。',
-      '编写程序化生成的地牢，来提高游戏的可玩性。',
-    ],
-  },
-  {
-    title: 'Buckeye Resale',
-    position: '团队项目组长',
-    link: '',
-    daterange: '六月 2021 - 七月 2021',
-    points: [
-      '设计了一个帮助大学生在线转售旧家具的网站。',
-      '使用 Ruby on Rails 和 SQLite 实现了购物城、用户界面和购物车。',
-      '使用 Devise 和 Stripe 构建用户注册和在线支付平台。',
+      '重制了经典游戏《塞尔达传说1986》的完整第一关。',
+      '利用状态、装饰器和单例设计模式设计了各种敌人和玩家能力。',
+      '通过编程实现了一个过程生成的地牢，以提高游戏的可玩性。',
     ],
   },
 ];
